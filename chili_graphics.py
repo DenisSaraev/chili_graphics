590a743a9d2d338fb7d69477d8aded3a042519ac
DenisSaraev <rads3@ya.ru>
2021-10-27 22:21:50 +0400

    changed plants

--- chili_graphics.py
+++ chili_graphics.py
@@ -26,12 +26,12 @@ logger.addHandler(ch)
 logger.info('Script started')
 
 #Plant info. We will use it for page's name
-chili_dict={'SoilMoisture-1':'null',
+chili_dict={'SoilMoisture-1':'Jalapeno_old_1',
     'SoilMoisture-2':'Jalapeno_2',
-    'SoilMoisture-3':'Jalapeno_old_3',
+    'SoilMoisture-3':'Habanero_3',
     'SoilMoisture-4':'CarolinaReaper_4',
     'SoilMoisture-5':'Jalapeno_5',
-    'SoilMoisture-6':'Tomato_6',}
+    'SoilMoisture-6':'Habanero_6',}
 
 #Excel file with data
 path_to_xlsx='/home/pi/projects/results/chili_graphics.xlsx'
@@ -56,12 +56,12 @@ for every_file in glob.glob(os.path.join('/home/pi/projects/results/','SoilMoist
     name=os.path.splitext(name)[0]
     name=chili_dict[name]
     logger.info(f'File for plant: {name}')
-    
+
     df=pd.read_csv(every_file,header=None,sep='|',names=['PLANT','DATE','SOIL MOISTURE'])
     logger.info(f'Converted to dataframe')
     df.to_excel(writer, sheet_name=name, index=False)
     worksheet=writer.sheets[name]
-    
+
     #Format settings
     header_format=workbook.add_format({
         'bold':True,
@@ -80,22 +80,22 @@ for every_file in glob.glob(os.path.join('/home/pi/projects/results/','SoilMoist
         'valign':'top',
         'num_format':'dd/mm/yyyy hh:mm',
         'border':1})
-    
+
     #Confirm settings to table
     worksheet.set_column('A:A', 8, text_format)
     worksheet.set_column('B:B', 19, date_format)
     worksheet.set_column('C:C', 18, text_format)
-    #Confirm settings to headers    
+    #Confirm settings to headers
     for col_num, value in enumerate(df.columns.values):
         worksheet.write(0,col_num,value,header_format)
     worksheet.autofilter('A1:C1')
     logger.info(f'Formating page completed')
-    
+
     #We need to count measurements for catch full table
     num_rows=df.count()[0]
     logger.info(f'Raws in file {num_rows}')
     #Chart for this plant
-    chart_soil.add_series({ 
+    chart_soil.add_series({
         'name':f'{name}', #Chart name
         'categories':f'={name}!$B$2:$B${num_rows+1}', #X-axis with date. 'num_rows+1' because first raw is header
         'values':f'={name}!$C$2:$C${num_rows+1}', }) #Y-axis with soil moisture
@@ -107,8 +107,8 @@ logger.info(f'Chart for soil moisture inserted on page')
 #New chart with themperature
 chart_themp = workbook.add_chart({'type': 'line'})
 #Settings for this graphic
-chart_themp.set_title ({'name':'Dynamic of themperature'}) 
-chart_themp.set_x_axis({'name':'DATE'}) 
+chart_themp.set_title ({'name':'Dynamic of themperature'})
+chart_themp.set_x_axis({'name':'DATE'})
 chart_themp.set_y_axis({'name':'THEMPERATURE,C'})
 chart_themp.set_size({'width': 1440, 'height': 640})
 
@@ -117,12 +117,12 @@ df=pd.read_csv(path_themp,header=None,sep='|',names=['SENSOR','DATE','THEMPERATU
 logger.info(f'Csv with themperature converted to dataframe')
 df.to_excel(writer, sheet_name='Themperature', index=False)
 worksheet=writer.sheets['Themperature']
-    
+
 #Confirm settings to table
 worksheet.set_column('A:A', 8, text_format)
 worksheet.set_column('B:B', 19, date_format)
 worksheet.set_column('C:C', 18, text_format)
-#Confirm settings to headers    
+#Confirm settings to headers
 for col_num, value in enumerate(df.columns.values):
     worksheet.write(0,col_num,value,header_format)
 worksheet.autofilter('A1:C1')
@@ -132,15 +132,15 @@ logger.info(f'Formating page with themperature completed')
 num_rows=df.count()[0]
 logger.info(f'Raws in file with themperature: {num_rows}')
 #Chart for this page
-chart_themp.add_series({ 
+chart_themp.add_series({
     'name':f'Themperature', #Chart name
     'categories':f'=Themperature!$B$2:$B${num_rows+1}', #X-axis with date. 'num_rows+1' because first raw is header
     'values':f'=Themperature!$C$2:$C${num_rows+1}', }) #Y-axis with themperature
 logger.info(f'Chart with themperature added')
 #Insert chart on first page and save document
 graph_sheet.insert_chart('A34', chart_themp)
-logger.info(f'Chart with themperature inserted on page')    
-    
+logger.info(f'Chart with themperature inserted on page')
+
 writer.save()
 logger.info(f'File saved')
 logger.info('Script finished')
